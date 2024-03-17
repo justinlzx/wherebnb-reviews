433abbe4d33abcb9d324ae875cbcddc3716d7cc2
justinlzx <justinlee01.jl@gmail.com>
2024-03-17 15:59:49 +0800

    put request to accoms to update listing

--- src/controller/review.controller.js
+++ src/controller/review.controller.js
@@ -1,34 +1,46 @@
 import { create, getAll, getAverageRating } from '../service/review.service.js';
+import axios from 'axios';
 
-export const createReview = async (req, res) => {
+export const createReview = async (req, res, next) => {
     const { review, listingId, userId, rating } = req.body;
 
     try {
-        const result = await create({
+        await create({
             review,
             listingId,
             userId,
             rating
+        })
+        .then(() => {
+            console.log('Review created successfully')
+        })
+        .catch((error) => {
+            next(error)
         });
 
         const newRating = await getAverageRating(listingId);
 
-        return res.status(201).json({
-            result,
-            newRating
+        await axios.put(`${process.env.ACCOMS_URL}/accoms/${listingId}`, {
+            rating: newRating.average
+        })
+        .then(() => {
+            return res.status(201).json('Review created and rating updated successfully');
+        })
+        .catch((error) => {
+            next(error)
         });
     }
     catch (error) {
-        throw new Error(error.message)
+       next(error)
     }
 };
 
-export const getAllReviews = async (req, res) => {
+export const getAllReviews = async (_, res, next) => {
     try {
         const result = await getAll();
         return res.status(200).json(result);
     }
     catch (error) {
-        throw new Error(error.message)
+       next(error)
     }
 };
